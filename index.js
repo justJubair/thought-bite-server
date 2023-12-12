879a8c57b18d38954d55fd9c0bce4bf9be989626
Jubair <jubair.ahmed7946@gmail.com>
2023-12-12 22:06:45 +0600

    Deployed: to vercel

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(process.env.DB_URI, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     
     // Database collection
     const messagesCollection = client.db("thoughtBiteDB").collection("messages")
@@ -43,7 +43,7 @@ async function run() {
     })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
